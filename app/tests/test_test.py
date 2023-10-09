a4283924c1eea862648cf0605a3bff4acaf9e614
Matt Spindler <matthew@spindler.uk>
2023-10-09 14:01:15 +0100

    Add tests for create_item

--- app/tests/test_test.py
+++ app/tests/test_test.py
@@ -1,18 +1,33 @@
 """Example pytest tests"""
 from app.main import app
 from fastapi.testclient import TestClient
-
+from pydantic_mongo import ObjectIdField
 
 client = TestClient(app)
 
-
-def test_answer():
+def test_create():
     """Example test use case"""
 
-    response = client.get("/test")
+    item_to_create = {
+    "name": "joes Bloggs",
+    "description": "This is joes item",
+    "price": 10,
+    "tax": 1.6
+    }
+
+    retrived_item = item_to_create
+
+    response = client.post("/test", json=item_to_create)
+    assert response.status_code == 201
+
+   
+    #Fetch th eitem with the return id to check it was created"
+    id = ObjectIdField(response.json()["_id"])
+
+    response = client.get(f"/test/{id}")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!"}
 
-
-
+    #Add the returned id to the retrived_Item
+    retrived_item["_id"] = id
+    assert response.json() == retrived_item
    
